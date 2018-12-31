56c33ef70045b9a7735d635fd4682f9c82fb6fdd
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-12-31 18:29:28 +0000

    Deploy Kayle7777/telescience to github.com/Kayle7777/telescience.git:gh-pages

---
+++ precache-manifest.f389b9da70dab042c5b61b51f4de078e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d4bd46423905a9e918dba279fe8c9f3c",
+    "url": "/telescience/static/media/space.d4bd4642.png"
+  },
+  {
+    "revision": "e715ac225bf066ac5059",
+    "url": "/telescience/static/js/runtime~main.e715ac22.js"
+  },
+  {
+    "revision": "e6b485d11df19433b01b",
+    "url": "/telescience/static/js/main.e6b485d1.chunk.js"
+  },
+  {
+    "revision": "b2ff0cd259bc384eb661",
+    "url": "/telescience/static/js/1.b2ff0cd2.chunk.js"
+  },
+  {
+    "revision": "e6b485d11df19433b01b",
+    "url": "/telescience/static/css/main.012b3d93.chunk.css"
+  },
+  {
+    "revision": "282656928773e9349a6cfbe72754ea9e",
+    "url": "/telescience/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/telescience/precache-manifest.543212a41f6f09f868f8e1aafb4afe39.js"
+  "/telescience/precache-manifest.f389b9da70dab042c5b61b51f4de078e.js"
 );
 
 workbox.clientsClaim();
