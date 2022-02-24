a537b31ed9e79099f612b06f54d108eb11f0a640
vortex1942 <davislachlan3@gmail.com>
2022-02-24 13:20:35 +0000

    Deploying to gh-pages from @ vortex1942/telescience@dc0f861bd515f24c2d7515c4e2efa4bc619df81b 🚀

--- precache-manifest.62490d3030a148114b6245fc346f7283.js
+++ precache-manifest.62490d3030a148114b6245fc346f7283.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "ff6f706186c3f9e49add",
+    "revision": "88c2cb51df64850abbf4",
     "url": "/telescience/static/css/main.e3f7f23b.chunk.css"
   },
   {
-    "revision": "ff6f706186c3f9e49add",
-    "url": "/telescience/static/js/main.ff330d27.chunk.js"
+    "revision": "88c2cb51df64850abbf4",
+    "url": "/telescience/static/js/main.f9e173c4.chunk.js"
   },
   {
     "revision": "7fb4757d5aa59309671c",
     "url": "/telescience/static/js/runtime~main.fc6f4c7c.js"
   },
   {
-    "revision": "c081648d251bbcef611b",
-    "url": "/telescience/static/js/2.af47ad9d.chunk.js"
+    "revision": "4ab8ec8af067093d20dc",
+    "url": "/telescience/static/js/2.338ae375.chunk.js"
   },
   {
     "revision": "d4bd46423905a9e918dba279fe8c9f3c",
     "url": "/telescience/static/media/space.d4bd4642.png"
   },
   {
-    "revision": "15306146d6f1f5c1a977ce9ac10765b3",
+    "revision": "8f22915775f0bd416013fa2891ed1b73",
     "url": "/telescience/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/telescience/precache-manifest.b9b39a682b06b36a0b87b3c48318acbc.js"
+  "/telescience/precache-manifest.62490d3030a148114b6245fc346f7283.js"
 );
 
 workbox.clientsClaim();
