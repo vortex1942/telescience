0e35aa922f64f93e37abb502e12821a8844eea5a
Vortex1942 <Vortex1942@users.noreply.github.com>
2022-10-11 23:07:58 +1100

    Added Support for Self Hosted Images

--- src/components/Images.js
+++ src/components/Images.js
@@ -19,20 +19,20 @@ const Images = ({ selectedMap, image }) => {
     );
     function mapInfo(selectedMap) {
         const info = {
-            cogmap1: `https://goonhub.com/images/maps/cogmap/z1`,
-            cogmap2: `https://goonhub.com/images/maps/cogmap2/z1`,
-            faintSignal: `https://goonhub.com/images/maps/cogmap2/z3`,
-            oshan: `https://goonhub.com/images/maps/oshan/z1`,
-            clarion: `https://goonhub.com/images/maps/clarion/z1`,
-            destiny: `https://goonhub.com/images/maps/destiny/z1`,
-            atlas: `https://goonhub.com/images/maps/atlas/z1`,
-            horizon: `https://goonhub.com/images/maps/horizon/z1`,
-            mushroom: `https://goonhub.com/images/maps/mushroom/z1`,
-            manta: `https://goonhub.com/images/maps/manta/z1`,
-            kondaru: `https://goonhub.com/images/maps/kondaru/z1`,
-            donut2: `https://goonhub.com/images/maps/donut2/z1`,
-            fleet: `https://goonhub.com/images/maps/bellerophon%20fleet/z1`,
-            donut3: `https://goonhub.com/images/maps/donut3/z1`,
+            cogmap1: `/images/cogmap1/`,
+            cogmap2: `/images/cogmap2/`,
+            faintSignal: `/images/z3/`,
+            oshan: `/images/oshan/`,
+            clarion: `/images/clarion/`,
+            destiny: `/images/destiny/`,
+            atlas: `/images/atlas/`,
+            horizon: `/images/horizon/`,
+            mushroom: `/images/mushroom/`,
+            manta: `/images/manta/`,
+            kondaru: `/images/kondaru/`,
+            donut2: `/images/donut2/`,
+            fleet: `/images/fleet/`,
+            donut3: `/images/donut3/`,
         };   
         if (!info[selectedMap]) return info['cogmap1'];
         return info[selectedMap];
