7e10ac58eb6e2ffe37426f48590fedb784a20485
Vortex1942 <Vortex1942@users.noreply.github.com>
2022-10-11 18:02:30 +1100

    File Renaming, Options & Comments

--- src/tools/PhotoProcessor.py
+++ src/tools/PhotoProcessor.py
@@ -1,6 +1,6 @@
 from select import select
 from PIL import Image, ImageDraw
-from os import walk, getcwd, path
+from os import walk, getcwd, path, listdir, rename
 
 #rootdir = getcwd()
 #imgdir = path.join(rootdir,"src","images","cogmap2")
@@ -9,27 +9,51 @@ from os import walk, getcwd, path
 #print(imgdir)
 #print(os.listdir(imgdir))
 
-rawimgdir = str(input("Root Directory of RAW images: "))
-imgdir = str(input("Directory for PROCESSED images:"))
+
+rawimgdir = "S:\\telescience\\src\\images\\cogmap1\\rawimages"
+#rawimgdir = str(input("Root Directory of RAW images: "))
 
 
+imgdir = "S:\\telescience\\src\\images\\cogmap1"
+#imgdir = str(input("Directory for PROCESSED images:"))
 
-SELexportfullimage = input("Save a full image? [Y/" f"\033[1mn\033[0m]: ")
+
+def func_imagerenamer():
+    x = y = 1
+    imagelist = [file for file in (sorted(listdir(rawimgdir), key=len)) if file.endswith('.png')]
+    for file_name in imagelist:
+        source = rawimgdir +"\\"+ file_name
+        new = rawimgdir +"\\"+ f"{RAWfilename}_{x}.png"
+        # Change new filename preset here ^^^^^^^^^^^ 
+        rename(source, new)
+        x+=1
+
+
+SELrenaming = str(input("Rename ALL RAW .png files to 'name_(X,Y).png' [y/" f"\033[1mN\033[0m]:"))
+if SELrenaming.lower() == "y" or SELrenaming.lower() == "yes":
+    RAWfilename = "test28"
+    RAWfilename = str(input("RAW image name: "))
+    func_imagerenamer()
+
+SELexportfullimage = "yes"
+#SELexportfullimage = input("Save a full image? [Y/" f"\033[1mn\033[0m]: ")
 if SELexportfullimage.lower() == "n" or SELexportfullimage.lower() == "no":
     SELexportfullimage = False
 else:
-    filename = str(input("Export full image filename: "))
+    exportfilename = "test28"
+    #exportfilename = str(input("filename for Full Image: "))
     SELexportfullimage = True
 
-#SELtransparency = "no"
-SELtransparency = input("Make Pink areas transparent?(Slow) [Y/" f"\033[1mn\033[0m]: ")
+
+SELtransparency = "no"
+#SELtransparency = input("Make Pink areas transparent?(Slow) [Y/" f"\033[1mn\033[0m]: ")
 if SELtransparency.lower() == "n" or SELtransparency.lower() == "no":
     SELtransparency = False
 else:
     SELtransparency = True
 
 
-#input("PAUSE")
+
 def func_transparancy():
     print("TRANSPARENCY PASS")
     data = masterexport.getdata()
@@ -42,24 +66,23 @@ def func_transparancy():
     masterexport.putdata(transparantdata)
     print("TRANSPARENCY PASS COMPLETE")
 
-def func_exportfullimage(imgdir,filename):
+def func_exportfullimage():
     print("SAVING FULL IMAGE")
-    file = (imgdir+"\\"+filename+".png")
+    file = (imgdir+"\\"+exportfilename+".png")
     print(file)
     file = open(file, "wb")
     masterexport.save(file) 
     print("FILE SAVED")
 
 
-
 print("DecompressionBombWarnings may occour due to image size")
 masterexport = Image.new("RGBA", (9600,9600), color=(255,255,255,255))
-lst = ["(16, 16, 1)","(46, 16, 1)","(76, 16, 1)","(106, 16, 1)","(136, 16, 1)","(166, 16, 1)","(196, 16, 1)","(226, 16, 1)","(256, 16, 1)","(286, 16, 1)","(16, 46, 1)","(46, 46, 1)","(76, 46, 1)","(106, 46, 1)","(136, 46, 1)","(166, 46, 1)","(196, 46, 1)","(226, 46, 1)","(256, 46, 1)","(286, 46, 1)","(16, 76, 1)","(46, 76, 1)","(76, 76, 1)","(106, 76, 1)","(136, 76, 1)","(166, 76, 1)","(196, 76, 1)","(226, 76, 1)","(256, 76, 1)","(286, 76, 1)","(16, 106, 1)","(46, 106, 1)","(76, 106, 1)","(106, 106, 1)","(136, 106, 1)","(166, 106, 1)","(196, 106, 1)","(226, 106, 1)","(256, 106, 1)","(286, 106, 1)","(16, 136, 1)","(46, 136, 1)","(76, 136, 1)","(106, 136, 1)","(136, 136, 1)","(166, 136, 1)","(196, 136, 1)","(226, 136, 1)","(256, 136, 1)","(286, 136, 1)","(16, 166, 1)","(46, 166, 1)","(76, 166, 1)","(106, 166, 1)","(136, 166, 1)","(166, 166, 1)","(196, 166, 1)","(226, 166, 1)","(256, 166, 1)","(286, 166, 1)","(16, 196, 1)","(46, 196, 1)","(76, 196, 1)","(106, 196, 1)","(136, 196, 1)","(166, 196, 1)","(196, 196, 1)","(226, 196, 1)","(256, 196, 1)","(286, 196, 1)","(16, 226, 1)","(46, 226, 1)","(76, 226, 1)","(106, 226, 1)","(136, 226, 1)","(166, 226, 1)","(196, 226, 1)","(226, 226, 1)","(256, 226, 1)","(286, 226, 1)","(16, 256, 1)","(46, 256, 1)","(76, 256, 1)","(106, 256, 1)","(136, 256, 1)","(166, 256, 1)","(196, 256, 1)","(226, 256, 1)","(256, 256, 1)","(286, 256, 1)","(16, 286, 1)","(46, 286, 1)","(76, 286, 1)","(106, 286, 1)","(136, 286, 1)","(166, 286, 1)","(196, 286, 1)","(226, 286, 1)","(256, 286, 1)","(286, 286, 1)",]
+imagelist = [file for file in sorted(listdir(rawimgdir), key=len) if file.endswith('.png')]
 x = ic = fc = 0
 per = 10
 y = 8640
-for p in lst:
-    file = rawimgdir+"\\"+str(lst[ic])+".png"
+for p in imagelist:
+    file = rawimgdir+"\\"+str(imagelist[ic])
     photo = Image.open(file).convert("RGBA") 
     if fc == 10:
         fc = x = 0
@@ -67,7 +90,8 @@ for p in lst:
         per += 10
         print(per,"%")
     #Verbose print
-    #print("iter: " f"{fc : >2}", "IMG XY: " f"{x : >4}", f"{y : >4}", "FILE: " f"{lst[ic] : >13}"      )
+
+    print("iter: " f"{fc : >2}", "IMG XY: " f"{x : >4}", f"{y : >4}", "FILE: " f"{imagelist[ic] : >13}"      )
     masterexport.paste(photo, (x, y))
     x+=960
     ic+=1
@@ -77,6 +101,6 @@ if SELtransparency == True:
     func_transparancy()
 
 if SELexportfullimage == True:
-    func_exportfullimage(imgdir, filename)
+    func_exportfullimage()
 
 
