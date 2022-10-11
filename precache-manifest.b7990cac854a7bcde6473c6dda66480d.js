31d9bb2def78c15987b883533a59ca16533f384f
vortex1942 <davislachlan3@gmail.com>
2022-10-11 12:35:00 +0000

    Deploying to gh-pages from @ vortex1942/telescience@6b088fd4d7f15e783c131730d1ba7d8fced32ff4 🚀

---
+++ precache-manifest.b7990cac854a7bcde6473c6dda66480d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5196bdd09733d9174d2d",
+    "url": "/telescience/static/css/main.e3f7f23b.chunk.css"
+  },
+  {
+    "revision": "5196bdd09733d9174d2d",
+    "url": "/telescience/static/js/main.34279c62.chunk.js"
+  },
+  {
+    "revision": "7fb4757d5aa59309671c",
+    "url": "/telescience/static/js/runtime~main.fc6f4c7c.js"
+  },
+  {
+    "revision": "d6ea7e0361b21d69dcbd",
+    "url": "/telescience/static/js/2.dcada47a.chunk.js"
+  },
+  {
+    "revision": "973ebe4f0300171eff04f8558f4f5c83",
+    "url": "/telescience/static/media/space.973ebe4f.png"
+  },
+  {
+    "revision": "f0a8edbedfe7f6d8f2f6999758a71135",
+    "url": "/telescience/index.html"
+  }
+];
