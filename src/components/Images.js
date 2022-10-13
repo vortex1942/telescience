df31c81d971280338859e1b0d6a97c9ebca7f9a6
Vortex1942 <Vortex1942@users.noreply.github.com>
2022-10-13 18:24:17 +1100

    Goonhub comments

--- src/components/Images.js
+++ src/components/Images.js
@@ -33,6 +33,23 @@ const Images = ({ selectedMap, image }) => {
             donut2: `images/donut2/`,
             //fleet: `/images/fleet/`,
             donut3: `images/donut3/`,
+            /*
+            //Leaving this here, Ideally Goonhub will be added to the CI/CD eventually so we don't need to update maps
+            cogmap1: `https://goonhub.com/images/maps/cogmap/z1`,
+            cogmap2: `https://goonhub.com/images/maps/cogmap2/z1`,
+            faintSignal: `https://goonhub.com/images/maps/cogmap2/z3`,
+            oshan: `https://goonhub.com/images/maps/oshan/z1`,
+            clarion: `https://goonhub.com/images/maps/clarion/z1`,
+            destiny: `https://goonhub.com/images/maps/destiny/z1`,
+            atlas: `https://goonhub.com/images/maps/atlas/z1`,
+            horizon: `https://goonhub.com/images/maps/horizon/z1`,
+            mushroom: `https://goonhub.com/images/maps/mushroom/z1`,
+            manta: `https://goonhub.com/images/maps/manta/z1`,
+            kondaru: `https://goonhub.com/images/maps/kondaru/z1`,
+            donut2: `https://goonhub.com/images/maps/donut2/z1`,
+            fleet: `https://goonhub.com/images/maps/bellerophon%20fleet/z1`,
+            donut3: `https://goonhub.com/images/maps/donut3/z1`,
+            */
         };   
         if (!info[selectedMap]) return info['cogmap1'];
         return info[selectedMap];
