e88f5076532cdb248e10db1430b0cff461c30b5f
vortex1942 <22815407+vortex1942@users.noreply.github.com>
2024-03-02 09:39:05 +1100

    Cleaner vars & input

--- src/tools/PhotoProcessor.py
+++ src/tools/PhotoProcessor.py
@@ -40,53 +40,49 @@ from PIL import Image
 from os import listdir, rename
 
 # Selection of Input/Output directories
-rawimgdir = str(input("Directory of RAW images: "))
-imgdir = str(input("Directory for PROCESSED images:"))
+raw_img_dir = str(input("Directory of RAW images: "))
+img_dir = str(input("Directory for PROCESSED images:"))
 
 
-# Selection for RAWimage renaming
-SELimagerenamer = str(
-    input("Rename ALL RAW .png files to '[name]_[INT].png' [Y/\033[1mN\033[0m]:")
+def get_user_choice(prompt):
+    choice = input(prompt).lower()
+    if choice in ["y", "yes"]:
+        return True
+    else:
+        return False
+
+
+rename_image = get_user_choice(
+    "Rename ALL RAW .png files to '[name]_[INT].png' [Y/\033[1mN\033[0m]:"
 )
-if SELimagerenamer.lower() == "y" or SELimagerenamer.lower() == "yes":
+if rename_image:
     RAWfilename = str(input("RAW image [name]: "))
-    SELimagerenamer = True
 
 
-# Selection for exporting a full size image
-SELexportfullimage = input("Save a full image? [Y/" "\033[1mN\033[0m]: ")
-if SELexportfullimage.lower() == "n" or SELexportfullimage.lower() == "no":
-    SELexportfullimage = False
-else:
+export_full_image = get_user_choice("Save a full image? [Y/\033[1mN\033[0m]:")
+if export_full_image:
     exportfilename = str(input("Filename for Full Image (E.g Cogmap1_full): "))
-    SELexportfullimage = True
 
-
-# Selection for applying transparency to purple areas (GoonhubSplices will be purple without this)
-SELtransparency = input("Make Pink areas transparent?(Slow) [Y/" "\033[1mN\033[0m]:")
-if SELtransparency.lower() == "n" or SELtransparency.lower() == "no":
-    SELtransparency = False
-else:
-    SELtransparency = True
-
-
-# Splice/Rename the image into Goonhub compatable images
-SELgoonhubsplice = input(
-    "Splice & Rename image into GoonHub formating? [Y/" "\033[1mN\033[0m]:"
+transparency = get_user_choice(
+    "Make Pink areas transparent?(Slow) [Y/\033[1mN\033[0m]:"
 )
-if SELgoonhubsplice.lower() == "y" or SELgoonhubsplice.lower() == "yes":
-    SELgoonhubsplice = True
+
+goonhub_splice = get_user_choice(
+    "Splice & Rename image into GoonHub formating? [Y/\033[1mN\033[0m]:"
+)
 
 
 # Function for renming RAW images to [NAME]_[INT].png, E.g Cogmap1_22.png
 def func_imagerenamer():
-    x = y = 1
+    x = 1
     imagelist = [
-        file for file in (sorted(listdir(rawimgdir), key=len)) if file.endswith(".png")
+        file
+        for file in (sorted(listdir(raw_img_dir), key=len))
+        if file.endswith(".png")
     ]
     for file_name in imagelist:
-        source = rawimgdir + "\\" + file_name
-        new = rawimgdir + "\\" + f"{RAWfilename}_{x}.png"
+        source = raw_img_dir + "\\" + file_name
+        new = raw_img_dir + "\\" + f"{RAWfilename}_{x}.png"
         # Change new filename preset here ^^^^^^^^^^^
         rename(source, new)
         x += 1
@@ -95,21 +91,21 @@ def func_imagerenamer():
 # Function for applying transparency to all purple areas[254,0,232] Will take 30 secs to process
 def func_transparancy():
     print("APPLYING TRANSPARANCY")
-    data = masterexport.getdata()
-    transparantdata = []
+    data = master_export.getdata()
+    transparancy_data = []
     for item in data:
         if item[0] == 254 and item[1] == 0 and item[2] == 232:
-            transparantdata.append((255, 255, 255, 0))
+            transparancy_data.append((255, 255, 255, 0))
         else:
-            transparantdata.append(item)
-    masterexport.putdata(transparantdata)
+            transparancy_data.append(item)
+    master_export.putdata(transparancy_data)
     print("TRANSPARENCY PASS COMPLETE")
 
 
 # Function for saving the image (params: name=Filename, export=Image IMG variable)
 def func_exportfullimage(name, export):
     print("SAVING IMAGE")
-    file = imgdir + "\\" + name + ".png"
+    file = img_dir + "\\" + name + ".png"
     print(file)
     file = open(file, "wb")
     export.save(file)
@@ -120,51 +116,53 @@ print("DecompressionBombWarnings may occour due to image size")
 
 
 # Where the magic happens, Creates a canvas and pastes RAWimages in a grid fashion
-masterexport = Image.new("RGBA", (9600,9600), color=(255,255,255,255))
-imagelist = [file for file in sorted(listdir(rawimgdir), key=len) if file.endswith('.png')]
+master_export = Image.new("RGBA", (9600, 9600), color=(255, 255, 255, 255))
+imagelist = [
+    file for file in sorted(listdir(raw_img_dir), key=len) if file.endswith(".png")
+]
 x = ic = fc = 0
 per = 10
 y = 8640
 # For loop stitches RAw images together
 for p in imagelist:
-    file = rawimgdir+"\\"+str(imagelist[ic])
-    photo = Image.open(file).convert("RGBA") 
+    file = raw_img_dir + "\\" + str(imagelist[ic])
+    photo = Image.open(file).convert("RGBA")
     if fc == 10:
         fc = x = 0
         y -= 960
         per += 10
-        print(per,"%")
-    #Verbose mode [Iteration]   [Image Coords]  [RAW Filename]
-    #print("iter: " f"{fc : >2}", "IMG XY: " f"{x : >4}", f"{y : >4}", "FILE: " f"{imagelist[ic] : >13}")
-    masterexport.paste(photo, (x, y))
-    x+=960
-    ic+=1
-    fc+=1
+        print(per, "%")
+    # Verbose mode [Iteration]   [Image Coords]  [RAW Filename]
+    # print("iter: " f"{fc : >2}", "IMG XY: " f"{x : >4}", f"{y : >4}", "FILE: " f"{imagelist[ic] : >13}")
+    master_export.paste(photo, (x, y))
+    x += 960
+    ic += 1
+    fc += 1
 
 
-#Function splices the large image into Goonhub Compatable images [1200x1200 w/ name=(x,y.png)]
+# Function splices the large image into Goonhub Compatable images [1200x1200 w/ name=(x,y.png)]
 def func_goonhubsplice():
     height = 9600 // 8
     width = 9600 // 8
     for i in range(0, 8):
         for j in range(0, 8):
             box = (j * width, i * height, (j + 1) * width, (i + 1) * height)
-            a = masterexport.crop(box)
+            a = master_export.crop(box)
             func_exportfullimage(f"{i},{j}", a)
 
 
 # Function calls according to previous user selections
-if SELimagerenamer:
+if rename_image:
     # Shouldn't matter if ran before or after processing
     func_imagerenamer()
 
-if SELtransparency:
+if transparency:
     func_transparancy()
 
-if SELexportfullimage:
-    func_exportfullimage(exportfilename, masterexport)
+if export_full_image:
+    func_exportfullimage(exportfilename, master_export)
 
-if SELgoonhubsplice:
+if goonhub_splice:
     func_goonhubsplice()
 
 
